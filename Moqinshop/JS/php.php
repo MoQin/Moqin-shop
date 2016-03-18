e94b7151fbdb65086cc48a319beaf0ee7db75ee7
MoQin <2860633476@qq.com>
2016-03-18 11:53:41 +0800

    318
    318
    
    Merge remote-tracking branch 'origin/master'
    
    Conflicts:
    
    317
    
    317
    
    workplace
    
    workplace
    
    318公司
    
    merge
    
    320宿舍
    
    320宿舍
    
    321company
    
    321company
    
    home321
    
    dorm0321
    
    company323
    
    Revert "company323"
    
    This reverts commit 0e20d315748faddbf2e01eb7914b583079d53895.
    
    公司图片323
    
    公司图片323
    
    dorm323
    
    添加首页js
    
    添加首页js
    
    company324
    
    company324
    
    company325
    
    company325
    
    0329conpany
    
    Update index.html
    
    0329company-2
    
    company0339

---
+++ Moqinshop/JS/php.php
@@ -0,0 +1,11 @@
+<?php 
+header("Content-Type:application/json;charset=utf-8"); 
+$json='{"d":[
+{"imgSrc":"<img src=\"./images/p1.jpg\">","title":"fdgdfg0","price":128.85},
+{"imgSrc":"<img src=\"./images/p2.jpg\">","title":"fdgdfg1","price":128.85},
+{"imgSrc":"<img src=\"./images/p3.jpg\">","title":"fdgdfg2","price":128.85}
+]}'; 
+
+	$callbackFn=$_GET['callback'];
+    echo "$callbackFn($json);" 
+?>
