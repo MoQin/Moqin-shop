4ab7a52be85f57385a3d964fc7bed57a0a24b2a0
MoQin <2860633476@qq.com>
2016-03-25 17:22:49 +0800

    company325
    company325

--- Moqinshop/JS/index.js
+++ Moqinshop/JS/index.js
@@ -1,11 +1,70 @@
-// 商品标题省略号
+
 $(function(){
+	// 商品标题省略号
 	$(".index-pro-title a").each(function(index){
-		console.log($(this).text().length)
 		var maxWidth=40;
 		if($(this).text().length>maxWidth){
 			$(this).text($(this).text().substring(0,maxWidth));
 			$(this).html($(this).html()+"....")
 		}
 	})
+	// 首页轮播
+	$(function() {
+	    var mySwiper = new Swiper(".swiper-container", {
+	        speed: 400,
+	        loop: true,
+	        pagination: ".swiper-pagination",
+	        autoplay: 2000
+	    })
+	})
+	// 首页加载更多
+	$(".pro-more").more({
+		"url":"http://115yz.com/moqin/php.php",
+		"template":".pro-more-list",
+		"trigger":".pro-more-pic",
+		"scroll":"true"
+	})
+	var lock=true;
+
+
+			    $.ajax({
+			        type: "post",
+			        dataType: "json",
+			        contentType: "text/json",
+			        url: "http://115yz.com/moqin/php.php",
+			        data: "{'pageSize':'00'}",
+			        success: function(data){
+			        	alert("d")
+			        },
+			        error:function(aa){
+			        	alert("错误");
+			        }
+
+		   		 });
+
+
+
+
+	// $(window).scroll(function() {
+			
+	// 		var scrollTop = $(window).scrollTop() + 100;
+	// 		var documentHeight = $(document).height() - $(window).height();
+	// 		if (scrollTop >= documentHeight &&lock==true) {
+				
+
+	// 			lock=false;
+	// 			$.ajax({
+	// 				type:"post",
+	// 				url:"http://115yz.com/moqin/php.php",
+	// 				data:{},
+	// 				dataType:"json",
+	// 				success:function(data){
+	// 						alert(data)
+	// 				}
+	// 			})
+	// 		}
+	// });
+
+
 })
+
---
+++ Moqinshop/JS/php.php
@@ -0,0 +1,7 @@
+
+
+
+<?php 
+header("Content-Type:application/json;charset=utf-8"); 
+echo('{"imgSrc":"./images/p1.jpg","title":"fdgdfg","price":128.85}'); 
+?>
